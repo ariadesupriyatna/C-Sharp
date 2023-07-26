d7f6ea5956b6136ea7c90f214119c67dfd7f8c89
code27 <88775244+ariadesupriyatna@users.noreply.github.com>
2023-07-26 09:30:31 +0700

    Add files via upload

---
+++ dasar.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		   var x = 27;
+		   var y = 72;
+		 
+		   if (x < y)
+		   { 
+			  Console.WriteLine("x Lebih kecil daripada y");
+		   }
+	}
+}
